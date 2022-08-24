c02e4a184028782a606149476ea911c7cfc1bdd3
jaewoo_byeon2 <jaewoo_byeon2@tmax.co.kr>
2022-08-24 18:15:38 +0900

    animation

--- src/pages/reservation/[shopId]/[menuId].tsx
+++ src/pages/reservation/[shopId]/[menuId].tsx
@@ -37,6 +37,19 @@ const ReservationMenuPage = () => {
   const totalPrice: number | null =
     optionPrice === undefined ? null : menu.price + optionPrice;
 
+  const selectedDateTime: dayjs.Dayjs | null =
+    selectedDate && selectedTime
+      ? dayjs(
+          new Date(
+            selectedDate.year,
+            selectedDate.month - 1,
+            selectedDate.day,
+            selectedTime.hour,
+            selectedTime.minute
+          )
+        )
+      : null;
+
   return (
     <div className="container mx-auto flex flex-col h-full p-4 gap-4">
       <ShopHeader shop={shop} to={`/reservation/${shopId}`} />
@@ -51,92 +64,145 @@ const ReservationMenuPage = () => {
             </div>
             <p className="text-sm text-gray-700">{menu.description}</p>
           </div>
-          <ul className="flex flex-col gap-2">
-            {menuOptions.map((option) => (
-              <li key={option.id}>
-                <div
-                  onClick={() => setSelectedOptionId(option.id)}
-                  className="flex flex-row justify-between"
-                >
-                  <div className="flex flex-row gap-2">
-                    <input
-                      type="radio"
-                      checked={option.id === selectedOptionId}
-                    />
-                    <span className="">{option.name}</span>
+          <hr />
+          <section className="flex flex-col gap-2">
+            <span className="text-2xl font-bold">옵션</span>
+            <ul className="flex flex-col gap-2">
+              {menuOptions.map((option) => (
+                <li key={option.id}>
+                  <div
+                    onClick={() => setSelectedOptionId(option.id)}
+                    className={`flex flex-row justify-between transition 
+                     ${
+                       option.id === selectedOptionId
+                         ? "text-gray-700"
+                         : "text-gray-300"
+                     }`}
+                  >
+                    <div className="flex flex-row gap-2">
+                      <input
+                        type="radio"
+                        checked={option.id === selectedOptionId}
+                      />
+                      <span className="">{option.name}</span>
+                    </div>
+                    <span className="tabular-nums">
+                      {option.price.toLocaleString()}원
+                    </span>
                   </div>
-                  <span
-                    className={`tabular-nums ${
-                      option.id === selectedOptionId
+                </li>
+              ))}
+            </ul>
+          </section>
+          {selectedOptionId !== null && <hr />}
+          {selectedOptionId === null ? (
+            <section className="transition-all duration-300 -translate-y-4 opacity-0" />
+          ) : (
+            <section className="flex flex-col gap-2 transition-all translate-y-0 opacity-100">
+              <span className="text-2xl font-bold">날짜</span>
+              <ul className="flex flex-col text-gray-300 gap-2">
+                {[
+                  { year: 2022, month: 8, day: 24 },
+                  { year: 2022, month: 8, day: 25 },
+                  { year: 2022, month: 8, day: 26 },
+                ].map(({ year, month, day }) => (
+                  <li
+                    key={`${year}${month}${day}`}
+                    onClick={() => setSelectedDate({ year, month, day })}
+                    className={`flex flex-row gap-2 transition ${
+                      selectedDate?.year === year &&
+                      selectedDate?.month === month &&
+                      selectedDate?.day === day
                         ? "text-gray-700"
-                        : "text-gray-300"
+                        : ""
                     }`}
                   >
-                    {option.price.toLocaleString()}원
-                  </span>
-                </div>
-              </li>
-            ))}
-          </ul>
-          {selectedOptionId !== null && (
-            <ul className="flex flex-col text-gray-300">
-              {[
-                { year: 2022, month: 8, day: 24 },
-                { year: 2022, month: 8, day: 25 },
-                { year: 2022, month: 8, day: 26 },
-              ].map(({ year, month, day }) => (
-                <li
-                  key={`${year}${month}${day}`}
-                  onClick={() => setSelectedDate({ year, month, day })}
-                  className={`${
-                    selectedDate?.year === year &&
-                    selectedDate?.month === month &&
-                    selectedDate?.day === day
-                      ? "text-gray-700"
-                      : ""
-                  }`}
-                >
-                  {year}년 {month}월 {day}일
-                </li>
-              ))}
-            </ul>
+                    <input
+                      type="radio"
+                      checked={
+                        selectedDate?.year === year &&
+                        selectedDate?.month === month &&
+                        selectedDate?.day === day
+                      }
+                    />
+                    {year}년 {month}월 {day}일
+                  </li>
+                ))}
+              </ul>
+            </section>
           )}
-          {selectedDate && (
-            <ul className="flex flex-col text-gray-300">
-              {[
-                { hour: 12, minute: 0 },
-                { hour: 14, minute: 0 },
-                { hour: 16, minute: 0 },
-              ].map(({ hour, minute }) => (
-                <li
-                  key={hour}
-                  onClick={() => setSelectedTime({ hour, minute })}
-                  className={`${
-                    selectedTime?.hour === hour &&
-                    selectedTime?.minute === minute
-                      ? "text-gray-700"
-                      : ""
-                  }`}
-                >
-                  {hour}:{String(minute).padStart(2, "0")}
-                </li>
-              ))}
-            </ul>
+          {selectedDate !== null && <hr />}
+          {selectedDate === null ? (
+            <section className="transition-all duration-300 -translate-y-4 opacity-0" />
+          ) : (
+            <section className="flex flex-col gap-2 transition-all translate-y-0 opacity-100">
+              <span className="text-2xl font-bold">시각</span>
+              <ul className="flex flex-col text-gray-300 gap-2">
+                {[
+                  { hour: 12, minute: 0 },
+                  { hour: 14, minute: 0 },
+                  { hour: 16, minute: 0 },
+                ].map(({ hour, minute }) => (
+                  <li
+                    key={hour}
+                    onClick={() => setSelectedTime({ hour, minute })}
+                    className={`flex flex-row gap-2 transition ${
+                      selectedTime?.hour === hour &&
+                      selectedTime?.minute === minute
+                        ? "text-gray-700"
+                        : ""
+                    }`}
+                  >
+                    <input
+                      type="radio"
+                      checked={
+                        selectedTime?.hour === hour &&
+                        selectedTime?.minute === minute
+                      }
+                    />
+                    {hour}:{String(minute).padStart(2, "0")} ~ {hour + 2}:
+                    {String(minute).padStart(2, "0")}
+                  </li>
+                ))}
+              </ul>
+            </section>
           )}
         </section>
-        <section className="flex flex-col gap-2 -m-4 p-4 shadow-t rounded">
-          <div className="flex flex-row justify-between text-xl font-bold">
-            <span>최종 가격</span>
-            {totalPrice ? (
-              <span>{totalPrice.toLocaleString()}원</span>
-            ) : (
-              <span>옵션을 선택해주세요.</span>
-            )}
-          </div>
+        <section className="flex flex-col gap-4 -m-4 p-4 shadow-t rounded">
+          <dl className="flex flex-col gap-1">
+            <div
+              className={`flex flex-row justify-between text-xl font-bold transition ${
+                totalPrice ? "text-gray-900" : "text-gray-300"
+              }`}
+            >
+              <dt>최종 가격</dt>
+              {totalPrice ? (
+                <dd className="tabular-nums">
+                  {totalPrice.toLocaleString()}원
+                </dd>
+              ) : (
+                <dd>옵션을 선택해주세요.</dd>
+              )}
+            </div>
+            <div
+              className={`flex flex-row justify-between ${
+                selectedDateTime ? "text-gray-900" : "text-gray-300"
+              }`}
+            >
+              <dt>일시</dt>
+              {selectedDateTime ? (
+                <dd className="tabular-nums">
+                  {selectedDateTime.format("YYYY년 M월 DD일 H:mm")}
+                </dd>
+              ) : (
+                <dd>일자를 선택해주세요.</dd>
+              )}
+            </div>
+          </dl>
           <button
-            className={`text-white bg-violet-500 disabled:bg-violet-200 py-2 rounded-md`}
+            className="text-white bg-violet-500 disabled:bg-violet-200 py-2 rounded-md transition"
             disabled={
-              !(selectedDate && selectedTime) && selectedOptionId !== null
+              !selectedDate || !selectedTime || selectedOptionId === null
             }
           >
             예약
